5824a5c53c32a9f5a953d6ad33e9d407ef01cf84
Ninzinhu <101211997+Ninzinhu@users.noreply.github.com>
2024-04-20 04:05:24 +0000

    exenumerico

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,33 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
+data_nascimento = '05-10-1976'
+idade_numerica = 46
+altura = 1.74
+print(type(idade_numerica))
+
+idade = 10 + 23
+print(idade)
 
 
 # Realize uma operação entre dados do tipo string e inteiro
 
+nome = "Sofia"
+nome * 10
+
+# string format
+
+f'Hoje paloma estudou {3 + 1} horas.'
+
+
 
 # Realize uma operação entre dados do tipo inteiro e float
+
+soma = idade + 2.3
+print (type(soma))
+divisão = 6/3
+print (divisão)
+
+decimal = 30/8
+print (decimal)
+
